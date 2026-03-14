0f35d92ad668a0de75e6b0289d6cf5fbd65da8be
Luke <suzuguenza@gmail.com>
2026-03-13 21:55:34 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Guzman Motos',
+  webDir: 'www'
+};
+
+export default config;
